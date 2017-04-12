9c008fa78b7f1f65ef211dc8079343a61dae88b7
Jeremiah Miller <j_miller429@yahoo.com>
2017-04-12 09:42:51 -0400

    Add project files.

---
+++ ElevenNote.Web1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElevenNote.Web1.Startup))]
+namespace ElevenNote.Web1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
