487782b03fbb9d7370d8918aa3b8d48cc4b51c05
Jeremiah Miller <j_miller429@yahoo.com>
2017-04-12 13:34:12 -0400

    added dbset

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -32,6 +32,9 @@ namespace ElevenNote.Data
             return new ApplicationDbContext();
         }
 
+        public DbSet<Note> Notes { get; set; }
+
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
